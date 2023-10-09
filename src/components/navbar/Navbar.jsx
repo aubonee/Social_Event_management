5cda7e7e412266c4ffc2ded85ac76de5da26172b
aubonee <chytahsin.2210@gmail.com>
2023-10-10 01:38:27 +0600

    blog section half done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -22,14 +22,14 @@ const Navbar = () => {
     <label tabIndex={0} className=" lg:hidden">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
     </label>
-    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-[#702632] rounded-box w-52">
     <li>
         <NavLink to="/" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "" }> Home </NavLink>
     </li>
 
-    <li>
-        <NavLink to="/Contact" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : ""}> Contact</NavLink>
-    </li>
+    <li>  <NavLink to="/Contact" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : ""}> Contact</NavLink></li>
+
+    <li>  <NavLink to="/Blogs" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : ""}> Blogs</NavLink></li>
 
    
     </ul>
@@ -45,6 +45,7 @@ const Navbar = () => {
  to="/" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? " text-bold underline underline-offset-8" : "" }> Home</NavLink>        </li>
                <li>  <NavLink to="/Contact" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "text-bold underline underline-offset-8" : ""}>Contact</NavLink></li>
                
+               <li>  <NavLink to="/Blogs" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? " text-bold underline underline-offset-8" : ""}> Blogs</NavLink></li>
 
 
                
--- src/pages/blog/Blogs.jsx
+++ src/pages/blog/Blogs.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <div className='w-3/4'>
+        <div className='w-full'>
+            <div className=' mx-auto w-3/5'>
                 <div>
-                    <div className="blog-item">
-                        <div>image</div>
-                        <div>
-                            <h1>nnnnnn</h1>
-                            <h4>mmmmmm</h4>
-                            <p>mmmmmmmm</p>
+                    <div className="blog-item flex">
+                        <div><img src="https://i.ibb.co/2cGwYxC/valiant-made-Urz-N-8-K1-PCE-unsplash.jpg" alt="" className='w-[850px] h-[250px]' /></div>
+                        <div className='ml-5'>
+                            <h1 className='font-bold text-4xl py-2 text-[#702632]'>The Art of Wedding Bliss</h1>
+                            <h4 className='font-semibold text-xl'>2023-10-15</h4>
+                            <h3 className=' font-semibold text-2xl'>Abdul Kader</h3>
+                            <p className='mt-5  text-justify text-xl'>Explore the enchanting world of weddings in 'The Art of Wedding Bliss.'
+                                 This blog post serves as your guide to creating the perfect dream wedding. 
+                                 From selecting breathtaking venues to choosing exquisite decor,
+                                 immerse yourself in the artistry of crafting unforgettable moments for your special day."</p>
                         </div>
                     </div>
                 </div>
@@ -19,4 +23,11 @@ const Blogs = () => {
     );
 };
 
+// {
+//     "id": 1,
+//     "title": "The Art of Wedding Bliss",
+//     "date": "2023-10-15",
+//     "author": "Your Name",
+//     "content": "Explore the enchanting world of weddings in 'The Art of Wedding Bliss.' This blog post serves as your guide to creating the perfect dream wedding. From selecting breathtaking venues to choosing exquisite decor, immerse yourself in the artistry of crafting unforgettable moments for your special day."
+//   },
 export default Blogs;
